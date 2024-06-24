eef00b8d3988b43f9f682bee364df00fb150143d
javeria-farooq <gettingeducation6@gmail.com>
2024-06-25 00:42:51 +0500

    Add uppercase and lowercase methods

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-let cities = ["karachi", "lahore", "islamabad", "quetta"]
+let cities = ["Karachi", "Lahore", "Islamabad", "Quetta"]
 let userCity = prompt("Enter Your City")
+
+let firstLetter = userCity.slice(0, 1).toUpperCase()
+let remainingLetter = userCity.slice(1).toLowerCase()
+let newUserCity = firstLetter + remainingLetter
+
 let found = false
 
+
 for(let i = 0; i < cities.length; i++) {
-    if(userCity === cities[i]) {
+    if(newUserCity === cities[i]) {
         found = true
-        console.log("found")
+        alert("Alert! City found...")
         break
     }
 }
 
 console.log(found) // agr array se match hgya t uske undr found ki true value hgi wo bahar bh true hi rhegi.
 if(found === false) {
-    console.log("not found")
+    alert("Oops! Your city is not found")
 }
